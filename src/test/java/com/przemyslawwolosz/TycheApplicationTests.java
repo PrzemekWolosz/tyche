fa3fc2e80da2714953e85db7170df8ea905f4443
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-11-14 10:01:43 +0100

    Initial commit

---
+++ src/test/java/com/przemyslawwolosz/TycheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przemyslawwolosz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TycheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
