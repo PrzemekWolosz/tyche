0789db623f3362757b180f4f0dadf976ccab3da1
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-11-14 11:51:42 +0100

    Add FileSystemStorage & update FileUploadController

--- src/main/java/com/przemyslawwolosz/module/media/FileUploadController.java
+++ src/main/java/com/przemyslawwolosz/module/media/FileUploadController.java
@@ -1,14 +1,33 @@
 package com.przemyslawwolosz.module.media;
 
+import com.przemyslawwolosz.module.media.storage.FileSystemStorage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class FileUploadController {
+
+    @Autowired
+    private FileSystemStorage storage;
 
     @GetMapping("/media")
     public String getMediaPage() {
         return "media/index.html";
     }
 
+    @PostMapping("/media")
+    public String saveFile(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes) {
+        storage.store(file);
+
+        redirectAttributes.addFlashAttribute(
+                "message",
+                "You successfully uploaded " + file.getOriginalFilename()
+        );
+        return "redirect:/media";
+    }
 }
